c782063688e4531e02dc957612ae0a68af7e9726
faeyzakhairunnisaibrahim-wq <faeyzakhairunnisa.ibrahim@gmail.com>
2026-06-05 15:38:40 +0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,36 @@
+let selectedFrame = 1;
 
+function selectFrame(frame, button){
+
+  selectedFrame = frame;
+
+  document
+    .querySelectorAll(".frame-btn")
+    .forEach(btn =>
+      btn.classList.remove("selected")
+    );
+
+  button.classList.add("selected");
+
+}
+
+window.addEventListener("load", () => {
+
+  document
+    .querySelector(".frame-btn")
+    .classList.add("selected");
+
+});
+
+document
+.querySelector(".start-btn")
+.addEventListener("click", function(){
+
+  localStorage.setItem(
+    "selectedFrame",
+    selectedFrame
+  );
+
+  window.location.href = "camera.html";
+
+});
